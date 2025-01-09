e6bd570caf2456097bf2f4ee8c05fea8fdf6d8dc
AMARDEEP KINI <92258265+Akamardeep@users.noreply.github.com>
2025-01-09 15:22:25 +0530

    Update main.tf

--- Aws_Modules/EC2/main.tf
+++ Aws_Modules/EC2/main.tf
@@ -11,11 +11,25 @@ resource "aws_instance" "this" {
 
   tags = var.tags
 }
+
+# Generate a new SSH private-public key pair
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create the key pair in AWS using the generated public key
 resource "aws_key_pair" "my_key_pair" {
-  key_name   = "my-key-pair"  # Specify the name for the key pair
-  public_key = null  # Path to your local public key
+  key_name   = "my-key-pair"
+  public_key = tls_private_key.example.public_key  # Use the generated public key
 
   tags = {
     Name = "MyKeyPair"
   }
+}
+
+# Optionally, store the private key on your local machine
+resource "local_file" "private_key" {
+  content  = tls_private_key.example.private_key_pem
+  filename = "${path.module}/my-key-pair.pem"  # Save private key to a file
 }
