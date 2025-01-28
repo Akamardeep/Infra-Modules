ef7775ec4c3af9d8c1cd92400d49d39e1b4404f8
AMARDEEP KINI <92258265+Akamardeep@users.noreply.github.com>
2025-01-28 23:37:16 +0530

    Update variables.tf

--- Aws_Modules/EC2/variables.tf
+++ Aws_Modules/EC2/variables.tf
@@ -1,41 +1,71 @@
 variable "region" {
-  description = "AWS region"
+  description = "AWS region to deploy resources"
   type        = string
   default     = "us-east-1"
 }
 
-variable "ami" {
-  description = "AMI ID for the instance"
+variable "project" {
+  description = "Project name for tagging resources"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the EC2 instance will be deployed"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where the EC2 instance will be deployed"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
   type        = string
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "Instance type for the EC2 instance"
   type        = string
   default     = "t2.micro"
 }
 
 variable "key_name" {
-  description = "Name of the key pair"
+  description = "Key pair name for the EC2 instance"
   type        = string
 }
 
-variable "subnet_id" {
-  description = "Subnet ID"
+variable "public_key" {
+  description = "Public key for SSH access"
   type        = string
 }
 
+variable "ssh_cidr_blocks" {
+  description = "CIDR blocks allowed for SSH access"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
 
+variable "http_cidr_blocks" {
+  description = "CIDR blocks allowed for HTTP access"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
 
-variable "associate_public_ip_address" {
-  description = "Whether to associate a public IP address with the instance"
+variable "associate_public_ip" {
+  description = "Associate a public IP with the EC2 instance"
   type        = bool
   default     = true
 }
 
-variable "tags" {
-  description = "Tags to apply to the instance"
-  type        = map(string)
-  default     = {}
+variable "root_volume_size" {
+  description = "Size of the root EBS volume (in GB)"
+  type        = number
+  default     = 8
 }
 
+variable "ebs_volume_size" {
+  description = "Size of the additional EBS volume (in GB)"
+  type        = number
+  default     = 20
+}
